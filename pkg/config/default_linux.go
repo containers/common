54e8b55d2f1e42c26cc46a71a715feefb0a4aeac
Sascha Grunert <sgrunert@suse.com>
2020-02-04 11:30:40 +0100

    Fix 32bit build by using unix.CGROUP2_SUPER_MAGIC
    Signed-off-by: Sascha Grunert <sgrunert@suse.com>

--- pkg/config/default_linux.go
+++ pkg/config/default_linux.go
@@ -2,18 +2,19 @@ package config
 
 import (
 	"syscall"
+
+	"golang.org/x/sys/unix"
 )
 
 // isCgroup2UnifiedMode returns whether we are running in cgroup2 mode.
 func isCgroup2UnifiedMode() (isUnified bool, isUnifiedErr error) {
-	_cgroup2SuperMagic := int64(0x63677270)
 	cgroupRoot := "/sys/fs/cgroup"
 
 	var st syscall.Statfs_t
 	if err := syscall.Statfs(cgroupRoot, &st); err != nil {
 		isUnified, isUnifiedErr = false, err
 	} else {
-		isUnified, isUnifiedErr = st.Type == _cgroup2SuperMagic, nil
+		isUnified, isUnifiedErr = st.Type == unix.CGROUP2_SUPER_MAGIC, nil
 	}
 	return
 }
