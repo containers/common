463fd3c3367763413c1026773139b9f8fd3636ba
Sascha Grunert <sgrunert@suse.com>
2020-08-28 21:35:01 +0200

    Add seccomp validation unit test for failing BuildProfile()
    This tests the last failure branch of the profile validation.
    
    Signed-off-by: Sascha Grunert <sgrunert@suse.com>

--- pkg/seccomp/validate_test.go
+++ pkg/seccomp/validate_test.go
@@ -25,6 +25,10 @@ func TestValidateProfile(t *testing.T) {
 			input:     `{"defaultAction": "SCMP_ACT_KILL", "architectures": ["SCMP_ARCH_X86"], "archMap": [{"architecture": "SCMP_ARCH_X86"}]}`,
 			shouldErr: true,
 		},
+		{ // BuildFilter failed
+			input:     `{"defaultAction": "SCMP_ACT_KILL", "architectures": ["SCMP_ARCH_X86"], "syscalls": [{ "name": "open" }]}`,
+			shouldErr: true,
+		},
 	} {
 		err := ValidateProfile(tc.input)
 		if tc.shouldErr {
