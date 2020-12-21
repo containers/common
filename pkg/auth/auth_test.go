3e530c4714732aa4304e5bc9c53a83dc0f3a899a
Daniel J Walsh <dwalsh@redhat.com>
2020-12-21 17:07:18 -0500

    Add support for DOCKER_CONFIG
    DOCKER_CONFIG environment variable is sometimes used to point
    to the config.json.  The container engines use REGISTRY_AUTH_FILE
    for similar functionality.  This PR causes programs that use
    GetAuthFile to use DOCKER_CONFIG if it set and REGISTRY_AUTH_FILE
    is not.
    
    Signed-off-by: Daniel J Walsh <dwalsh@redhat.com>

---
+++ pkg/auth/auth_test.go
@@ -0,0 +1,65 @@
+package auth
+
+import (
+	"io/ioutil"
+	"os"
+
+	. "github.com/onsi/ginkgo"
+	"github.com/onsi/gomega"
+)
+
+var _ = Describe("Config", func() {
+
+	Describe("ValidateAuth", func() {
+		It("validate GetDefaultAuthFile", func() {
+			// Given
+			oldDockerConf, envDockerSet := os.LookupEnv("DOCKER_CONFIG")
+			os.Setenv("DOCKER_CONFIG", "/tmp/docker.file")
+			oldConf, envSet := os.LookupEnv("REGISTRY_AUTH_FILE")
+			os.Setenv("REGISTRY_AUTH_FILE", "/tmp/registry.file")
+			// When			// When
+			authFile := GetDefaultAuthFile()
+			// Then
+			gomega.Expect(authFile).To(gomega.BeEquivalentTo("/tmp/registry.file"))
+			os.Unsetenv("REGISTRY_AUTH_FILE")
+
+			// Fall back to DOCKER_CONFIG
+			authFile = GetDefaultAuthFile()
+			// Then
+			gomega.Expect(authFile).To(gomega.BeEquivalentTo("/tmp/docker.file"))
+			os.Unsetenv("DOCKER_CONFIG")
+
+			// Fall back to DOCKER_CONFIG
+			authFile = GetDefaultAuthFile()
+			// Then
+			gomega.Expect(authFile).To(gomega.BeEquivalentTo(""))
+
+			// Undo that
+			if envSet {
+				os.Setenv("REGISTRY_AUTH_FILE", oldConf)
+			}
+			if envDockerSet {
+				os.Setenv("DOCKER_CONFIG", oldDockerConf)
+			}
+		})
+	})
+
+	It("validate CheckAuthFile", func() {
+		// When			// When
+		err := CheckAuthFile("")
+		// Then
+		gomega.Expect(err).To(gomega.BeNil())
+
+		conf, _ := ioutil.TempFile("", "authfile")
+		defer os.Remove(conf.Name())
+		// When			// When
+		err = CheckAuthFile(conf.Name())
+		// Then
+		gomega.Expect(err).To(gomega.BeNil())
+
+		// When			// When
+		err = CheckAuthFile(conf.Name() + "missing")
+		// Then
+		gomega.Expect(err).ShouldNot(gomega.BeNil())
+	})
+})
