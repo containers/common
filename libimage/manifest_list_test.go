0f93c92b75069b5f6abf8ededbf05737703ae70d
flouthoc <flouthoc.git@gmail.com>
2021-08-27 12:58:46 +0530

    runtime: Add ReturnManifestIfPresent to LookupImageOptions
    If matching images resolves to a manifest list, return manifest list
    instead of resolving to image instance, if manifest list is not found
    try resolving image.
    
    Signed-off-by: Aditya Rajan <flouthoc.git@gmail.com>

--- libimage/manifest_list_test.go
+++ libimage/manifest_list_test.go
@@ -38,3 +38,40 @@ func TestCreateManifestList(t *testing.T) {
 	require.Error(t, err)
 	require.Equal(t, errors.Cause(err), ErrNotAManifestList)
 }
+
+// Following test ensure that `Tag` tags the manifest list instead of resolved image.
+// Both the tags should point to same image id
+func TestCreateAndTagManifestList(t *testing.T) {
+
+	tagName := "testlisttagged"
+	listName := "testlist"
+	runtime, cleanup := testNewRuntime(t)
+	defer cleanup()
+	ctx := context.Background()
+
+	list, err := runtime.CreateManifestList(listName)
+	require.NoError(t, err)
+	require.NotNil(t, list)
+
+	manifestListOpts := &ManifestListAddOptions{All: true}
+	_, err = list.Add(ctx, "docker://busybox", manifestListOpts)
+	require.NoError(t, err)
+
+	list, err = runtime.LookupManifestList(listName)
+	require.NoError(t, err)
+	require.NotNil(t, list)
+
+	lookupOptions := &LookupImageOptions{ManifestList: true}
+	image, _, err := runtime.LookupImage(listName, lookupOptions)
+	require.NoError(t, err)
+	require.NotNil(t, image)
+	err = image.Tag(tagName)
+	require.NoError(t, err, "tag should have succeeded: %s", tagName)
+
+	taggedImage, _, err := runtime.LookupImage(tagName, lookupOptions)
+	require.NoError(t, err)
+	require.NotNil(t, taggedImage)
+
+	// Both origin list and newly tagged list should point to same image id
+	require.Equal(t, image.ID(), taggedImage.ID())
+}
