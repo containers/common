d033dd7a3fa72f17e34778b53300b7c93e29f393
Paul Holzinger <pholzing@redhat.com>
2022-02-14 16:23:21 +0100

    libnetwork: fix NextNetwork() to support arbitrary CIDR
    NextSubnet() has to work for more than a, b, c class networks. We also
    have to increment arbitrary subnets when we add support for a new config
    field where users can specify this.
    
    Signed-off-by: Paul Holzinger <pholzing@redhat.com>

--- libnetwork/internal/util/ip_test.go
+++ libnetwork/internal/util/ip_test.go
@@ -22,8 +22,20 @@ func TestNextSubnet(t *testing.T) {
 		want    *net.IPNet
 		wantErr bool
 	}{
+		{"class a", args{subnet: parseCIDR("10.0.0.0/8")}, parseCIDR("11.0.0.0/8"), false},
 		{"class b", args{subnet: parseCIDR("192.168.0.0/16")}, parseCIDR("192.169.0.0/16"), false},
 		{"class c", args{subnet: parseCIDR("192.168.1.0/24")}, parseCIDR("192.168.2.0/24"), false},
+		{"custom cidr /23", args{subnet: parseCIDR("192.168.0.0/23")}, parseCIDR("192.168.2.0/23"), false},
+		{"custom cidr /23 2", args{subnet: parseCIDR("192.168.2.0/23")}, parseCIDR("192.168.4.0/23"), false},
+		{"custom cidr /23 with overflow", args{subnet: parseCIDR("192.168.254.0/23")}, parseCIDR("192.169.0.0/23"), false},
+		{"/9", args{subnet: parseCIDR("10.0.0.0/9")}, parseCIDR("10.128.0.0/9"), false},
+		{"/11", args{subnet: parseCIDR("10.0.0.0/11")}, parseCIDR("10.32.0.0/11"), false},
+		{"/11", args{subnet: parseCIDR("0.0.0.0/11")}, parseCIDR("0.32.0.0/11"), false},
+		{"only one subnet", args{subnet: parseCIDR("0.0.0.0/0")}, nil, true},
+		{"no more subnets", args{subnet: parseCIDR("255.255.255.0/24")}, nil, true},
+		{"no more subnets 2", args{subnet: parseCIDR("255.240.0.0/12")}, nil, true},
+		{"ipv6", args{subnet: parseCIDR("fdfd:d319:b145:fd4b::/64")}, parseCIDR("fdfd:d319:b145:fd4c::/64"), false},
+		{"ipv6 with overflow", args{subnet: parseCIDR("fdfd:d319:b145:ffff::/64")}, parseCIDR("fdfd:d319:b146:0::/64"), false},
 	}
 	for _, tt := range tests {
 		test := tt
