8f4d13b90d30fc996f7c77c800587b3975225206
Daniel J Walsh <dwalsh@redhat.com>
2023-01-11 09:55:51 -0500

    Compile regex on demand not in init
    Should speed up app startup time a little since the compile
    happens for all users of the library. Compile only on use.
    
    [NO NEW TESTS NEEDED]
    
    Signed-off-by: Daniel J Walsh <dwalsh@redhat.com>

--- pkg/configmaps/configmaps.go
+++ pkg/configmaps/configmaps.go
@@ -5,12 +5,12 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
-	"regexp"
 	"strings"
 	"time"
 
 	"github.com/containers/common/pkg/configmaps/filedriver"
 	"github.com/containers/storage/pkg/lockfile"
+	"github.com/containers/storage/pkg/regexp"
 	"github.com/containers/storage/pkg/stringid"
 )
 
@@ -49,7 +49,7 @@ var configMapsFile = "configmaps.json"
 
 // configMapNameRegexp matches valid configMap names
 // Allowed: 64 [a-zA-Z0-9-_.] characters, and the start and end character must be [a-zA-Z0-9]
-var configMapNameRegexp = regexp.MustCompile(`^[a-zA-Z0-9][a-zA-Z0-9_.-]*$`)
+var configMapNameRegexp = regexp.Delayed(`^[a-zA-Z0-9][a-zA-Z0-9_.-]*$`)
 
 // ConfigMapManager holds information on handling configmaps
 type ConfigMapManager struct {
