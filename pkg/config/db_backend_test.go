e7f576458d84380eb4dfc8aad039187de29a6020
Valentin Rothberg <vrothberg@redhat.com>
2023-02-20 11:08:04 +0100

    containers.conf: add database backend option
    The on-going work of adding a new database backend to Podman requires it
    to be configurable in containers.conf.  To prevent any breaking changes,
    the backend defaults to Podman's current boltdb backend.  Also export
    the parsing function for it be usable by Podman.
    
    NOTE: documentation will be added once the database rewrite in Podman
    has been completed.  I want to avoid any incomplete docs from leaking
    into a release.
    
    Fixes: https://issues.redhat.com/browse/RUN-1772
    Signed-off-by: Valentin Rothberg <vrothberg@redhat.com>

---
+++ pkg/config/db_backend_test.go
@@ -0,0 +1,33 @@
+package config
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestDBBackend(t *testing.T) {
+	tests := []struct {
+		input    string
+		valid    bool
+		expected DBBackend
+	}{
+		{stringBoltDB, true, DBBackendBoltDB},
+		{stringSQLite, true, DBBackendSQLite},
+		{"", false, DBBackendUnsupported},
+		{stringSQLite + " ", false, DBBackendUnsupported},
+	}
+
+	for _, test := range tests {
+		result, err := ParseDBBackend(test.input)
+		if test.valid {
+			require.NoError(t, err, "should parse %v", test)
+			require.NoError(t, result.Validate(), "should validate %v", test)
+			require.Equal(t, test.expected, result)
+		} else {
+			require.Error(t, err, "should NOT parse %v", test)
+			require.Error(t, result.Validate(), "should NOT validate %v", test)
+			require.Equal(t, test.expected, result)
+		}
+	}
+}
