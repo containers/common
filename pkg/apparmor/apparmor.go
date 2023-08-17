06e66516984d4baa86e0d338650fdbe4c4fba846
Valentin Rothberg <vrothberg@redhat.com>
2023-08-17 10:16:34 +0200

    Revert "Revert "Revert "Allow rootless containers to use AppArmor profiles"""
    This reverts commit 8ec093a61783bd50ada9fd521564cd8111c49a71 because it
    was merged prematurely.
    
    Signed-off-by: Valentin Rothberg <vrothberg@redhat.com>

--- pkg/apparmor/apparmor.go
+++ pkg/apparmor/apparmor.go
@@ -20,4 +20,6 @@ const (
 var (
 	// ErrApparmorUnsupported indicates that AppArmor support is not supported.
 	ErrApparmorUnsupported = errors.New("AppArmor is not supported")
+	// ErrApparmorRootless indicates that AppArmor support is not supported in rootless mode.
+	ErrApparmorRootless = errors.New("AppArmor is not supported in rootless mode")
 )
