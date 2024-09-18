dde767242abe087f67f1c88369694f71132d8a26
Kir Kolyshkin <kolyshkin@gmail.com>
2024-09-18 16:31:55 -0700

    pkg/capabilities: rm RHEL < 6.7 compat hack
    Kernel 2.6.32-573.el6, released 2015-07-22 as part of RHEL 6.7,
    supports cap_last_cap file, so there is no need for a workaround.
    
    Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>

--- pkg/capabilities/capabilities.go
+++ pkg/capabilities/capabilities.go
@@ -41,10 +41,6 @@ func getCapName(c capability.Cap) string {
 
 func init() {
 	last := capability.CAP_LAST_CAP
-	// hack for RHEL6 which has no /proc/sys/kernel/cap_last_cap
-	if last == capability.Cap(63) {
-		last = capability.CAP_BLOCK_SUSPEND
-	}
 	for _, cap := range capability.List() {
 		if cap > last {
 			continue
